8b95e5099d187181689a37f6c0a360f849613eb7
Elis Gen <imperia1991@gmail.com>
2014-11-02 17:09:07 +0200

    commit

--- protected/modules/admin/views/search/index.php
+++ protected/modules/admin/views/search/index.php
@@ -2,14 +2,14 @@
     <h4>Настройки поиска</h4>
 </div>
 <div class="row">
-    <?php
+    <?php /*
     $this->widget('bootstrap.widgets.TbButton', array(
         'label' => 'Обновить индексы',
         'type' => null, // null, 'primary', 'info', 'success', 'warning', 'danger' or 'inverse'
         'size' => 'small', // null, 'large', 'small' or 'mini'
         'url' => Yii::app()->createUrl('/admin/search/create'),
     ));
-    ?>
+    */ ?>
 </div>
 <div class="row" style="margin-top: 10px">
     <?php
