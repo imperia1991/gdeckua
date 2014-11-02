28f000f676b744c40989f737519ea3513da6b17b
Elis Gen <imperia1991@gmail.com>
2014-11-02 14:02:03 +0200

    release

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -27,18 +27,18 @@
     <script src="/js/pace.min.js"></script>
     <script type="text/javascript" src="/js/jquery.jgrowl.js"></script>
     <script type="text/javascript" src="/js/feedback.js"></script>
-<!--    <script type="text/javascript" src="/js/auth.js"></script>-->
+    <script type="text/javascript" src="/js/auth.js"></script>
 
-<!--    <script>-->
-<!--        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){-->
-<!--        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),-->
-<!--        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)-->
-<!--        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');-->
-<!---->
-<!--        ga('create', 'UA-50948800-1', 'gde.ck.ua');-->
-<!--        ga('send', 'pageview');-->
-<!---->
-<!--    </script>-->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-50948800-1', 'gde.ck.ua');
+        ga('send', 'pageview');
+
+    </script>
 
     <link rel="icon" type="image/png" href="<?php echo Yii::app()->request->baseUrl; ?>/img/favicon.png" />
     <base href="<?php echo Yii::app()->baseUrl; ?>">
@@ -59,6 +59,7 @@
 
                         <section class="top-bar-section">
                             <!-- Right Nav Section -->
+                            <?php /* ?>
                             <ul class="right">
                                 <?php if (Yii::app()->user->isGuest):  ?>
                                     <li><a href="<?php echo Yii::app()->createUrl('/' . Yii::app()->getLanguage() . '/signin'); ?>" class="signin"><?php echo Yii::t('main', 'Войти'); ?></a></li>
@@ -72,6 +73,7 @@
                                     <li><a href="<?php echo Yii::app()->createUrl('/' . Yii::app()->getLanguage() . '/logout'); ?>" class="logout"><?php echo Yii::t('main', 'Выйти'); ?></a></li>
                                 <?php endif; ?>
                             </ul>
+                            <?php */ ?>
                             <div class="right currency">
                                 <?php //$this->renderPartial('/partials/_social'); ?>
                             </div>
@@ -300,30 +302,30 @@
 
 <!-- .footer -->
 <!-- Yandex.Metrika counter-->
-<!--<script type="text/javascript">-->
-<!--(function (d, w, c) {-->
-<!--    (w[c] = w[c] || []).push(function() {-->
-<!--        try {-->
-<!--            w.yaCounter24984920 = new Ya.Metrika({id:24984920,-->
-<!--                    clickmap:true,-->
-<!--                    trackLinks:true,-->
-<!--                    accurateTrackBounce:true});-->
-<!--        } catch(e) { }-->
-<!--    });-->
-<!---->
-<!--    var n = d.getElementsByTagName("script")[0],-->
-<!--        s = d.createElement("script"),-->
-<!--        f = function () { n.parentNode.insertBefore(s, n); };-->
-<!--    s.type = "text/javascript";-->
-<!--    s.async = true;-->
-<!--    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";-->
-<!---->
-<!--    if (w.opera == "[object Opera]") {-->
-<!--        d.addEventListener("DOMContentLoaded", f, false);-->
-<!--    } else { f(); }-->
-<!--})(document, window, "yandex_metrika_callbacks");-->
-<!--</script>-->
-<!--<noscript><div><img src="//mc.yandex.ru/watch/24984920" style="position:absolute; left:-9999px;" alt="" /></div></noscript>-->
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter24984920 = new Ya.Metrika({id:24984920,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+        } catch(e) { }
+    });
+
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/24984920" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 </body>
 </html>
